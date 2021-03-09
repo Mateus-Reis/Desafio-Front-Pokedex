665f909fad538a2abf41965c8338bf19109bbf2c
mateusreisbefore <mateus.ficagna@before.com.br>
2021-03-09 18:33:49 -0400

    Formatação Código

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-          <img src="./assets/beforeLogo.png">
-         
-          <hr>
-          <h4 class="is-size">Pokedex Before </h4>
-          <input type="text" id="Buscar Pokemon Nome" v-model="textoBuscado" class="input is-rounded">
+      <img src="./assets/beforeLogo.png" />
 
-        <div class="columns is-multiline is-mobile is-centered"> 
-      <div class="column is-one-third" v-for="(poke, index) in resultadoBusca" :key="index">
-        <Pokemon
-          :textoBuscado="textoBuscado"
-          :name="poke.name"
-          :url="poke.url"
-          :num="poke.num"
-          :type="poke.type"
-          :abilities="poke.name"
-        />
-      </div>
+      <hr />
+      <h4 class="is-size">Pokedex Before</h4>
+      <input
+        type="text"
+        id="Buscar Pokemon Nome"
+        v-model="textoBuscado"
+        class="input is-rounded"
+      />
+
+      <div class="columns is-multiline is-mobile is-centered">
+        <div
+          class="column is-one-third"
+          v-for="(poke, index) in resultadoBusca"
+          :key="index"
+        >
+          <Pokemon
+            :textoBuscado="textoBuscado"
+            :name="poke.name"
+            :url="poke.url"
+            :num="poke.num"
+            :type="poke.type"
+            :abilities="poke.name"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -32,44 +41,46 @@ export default {
   data() {
     return {
       pokemons: [],
-      busca: ''
+      busca: "",
     };
   },
-  created: function () {
+  created: function() {
     axios
       .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
       .then((res) => {
         console.log("Carregou a lista de pokemons :)");
         const pokemonsWithId = res.data.results;
         this.pokemons = pokemonsWithId.map((item, index) => {
-          return {...item, num: index +1}
-        })
+          return { ...item, num: index + 1 };
+        });
       });
   },
   components: {
     Pokemon,
   },
   computed: {
-
     textoBuscado: {
-      get () {
+      get() {
         return this.busca;
       },
-      set (value) {
+      set(value) {
         this.busca = value;
-      }
+      },
     },
 
-    resultadoBusca: function (){
-      if(this.busca == '' || this.busca == ' '){
-          return this.pokemons;
-      }else{
-        let busca = this.pokemons.filter(({ name, num }) => name === this.busca || Number(num) === Number(this.busca))
-          console.log(busca);
-          return busca;
+    resultadoBusca: function() {
+      if (this.busca == "" || this.busca == " ") {
+        return this.pokemons;
+      } else {
+        let busca = this.pokemons.filter(
+          ({ name, num }) =>
+            name === this.busca || Number(num) === Number(this.busca)
+        );
+        console.log(busca);
+        return busca;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -82,5 +93,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="pokemon">
-
     <div class="card" @click="openModal()">
       <div class="card-image">
         <figure>
@@ -15,65 +14,64 @@
         </div>
 
         <div class="content">
-            <button class="button is-fullwidth" @click="mudarSprite">Abrir Pokemon</button>
+          <button class="button is-fullwidth" @click="mudarSprite">
+            Abrir Pokemon
+          </button>
         </div>
       </div>
     </div>
 
-    <div class="modal" :class="{'is-active': modalActived}">
-       <div class="modal-background"></div>
-       <div class="modal-content">
-         
-           <div class="card">
-      <div class="card-image">
-        <figure>
-          <img :src="currentImg" alt="Placeholder image" />
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="subtitle is-5">{{ this.pokemon.type }}</p>
-            <p class="subtitle is-5">{{ this.pokemon.ability }}</p>
-            <p class="subtitle is-5">{{ this.pokemon.height }}</p>
-            <p class="subtitle is-5">{{ this.pokemon.weight }}</p>
+    <div class="modal" :class="{ 'is-active': modalActived }">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="card">
+          <div class="card-image">
+            <figure>
+              <img :src="currentImg" alt="Placeholder image" />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="subtitle is-5">{{ this.pokemon.type }}</p>
+                <p class="subtitle is-5">{{ this.pokemon.ability }}</p>
+                <p class="subtitle is-5">{{ this.pokemon.height }}</p>
+                <p class="subtitle is-5">{{ this.pokemon.weight }}</p>
+              </div>
+            </div>
 
-
+            <div class="content">
+              <button class="button is-fullwidth" @click="mudarSprite">
+                Girar Pokemon :)
+              </button>
+            </div>
           </div>
         </div>
-
-        <div class="content">
-            <button class="button is-fullwidth" @click="mudarSprite">Girar Pokemon :)</button>
-        </div>
       </div>
+      <button class="modal-close" @click="closeModal()"></button>
     </div>
-         
-       </div>
-       <button class="modal-close" @click="closeModal()"></button> 
-     </div>
-   </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-
-watch: {
-    textoBuscado: function ()  {
+  watch: {
+    textoBuscado: function() {
       console.log(this.pokemon);
       this.chamarPokemon();
     },
   },
 
-  created: function () {
+  created: function() {
     this.chamarPokemon();
   },
   data() {
     return {
-        modalActived: false,
-        isFront: true,
-        currentImg: '',
+      modalActived: false,
+      isFront: true,
+      currentImg: "",
       pokemon: {
         type: "",
         front: "",
@@ -88,48 +86,46 @@ watch: {
     textoBuscado: String,
     num: Number,
     name: String,
-    url: String
+    url: String,
   },
 
   methods: {
+    openModal() {
+      this.modalActived = true;
+    },
 
-      openModal () {
-          this.modalActived = true;
-      },
+    closeModal() {
+      this.modalActived = false;
+    },
 
-      closeModal () {
-        this.modalActived = false;
-      },
-
-      chamarPokemon () {
-        axios.get(this.url).then((res) => {
+    chamarPokemon() {
+      axios.get(this.url).then((res) => {
         this.pokemon.type = "Tipo: " + res.data.types[0].type.name;
-        this.pokemon.ability = "Habilidade: " + res.data.abilities[0].ability.name;
+        this.pokemon.ability =
+          "Habilidade: " + res.data.abilities[0].ability.name;
         this.pokemon.height = "Altura: " + res.data.height + " M";
         this.pokemon.weight = "Peso: " + res.data.weight + " Kg";
         this.pokemon.front = res.data.sprites.front_default;
         this.pokemon.back = res.data.sprites.back_default;
         this.currentImg = this.pokemon.front;
-      
-    });
-    console.log(this.url);
-      },
-      mudarSprite () {
-          if(this.isFront){
-              this.isFront = false;
-              this.currentImg = this.pokemon.back;
-          }else {
-              this.isFront = true;
-              this.currentImg = this.pokemon.front;
-          }
+      });
+      console.log(this.url);
+    },
+    mudarSprite() {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      } else {
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
       }
-  }
+    },
+  },
 };
-
 </script>
 
 <style>
 #pokemon {
-    margin-top: 2%;
+  margin-top: 2%;
 }
 </style>
